12fecabced00bc2dd4084a2df1f2c88e6fb5fff0
Diogo Ferreira da Costa Patrao <djogopatrao@gmail.com>
2018-03-07 14:00:23 -0300

    fixing wrong call to reindex

--- src/sigtap_tuss_tf_idf.py
+++ src/sigtap_tuss_tf_idf.py
@@ -10,7 +10,7 @@ import datetime
 from sklearn.model_selection import train_test_split
 import random
 import pickle
-
+import numpy as np
 
 
 def read_data():
@@ -18,6 +18,20 @@ def read_data():
     file = pd.read_csv("./tuss_para_sigtap.txt", delimiter="\t", names=['codigo_tuss', 'texto_tuss', 'codigo_sigtap', 'texto_sigtap'], encoding='utf-8')
     file2 = pd.read_csv("./tuss_tab_full.txt", delimiter="\t", names=['codigo', 'cap', 'grupo', 'subgrupo', 'seq', 'digito', 'nome_cap', 'nome_grupo', 'nome_subgrupo', 'procedimento'])
 
+    return file,file2
+
+
+def resample_input_file( my_file, train_size=0.9 ):
+
+    # fazer o sampling aqui apesar de VARZEA
+    print( "%s shuffling the input file (lazy but workey)" % ( str(datetime.datetime.now()) ) )
+    tmp = my_file.reindex( np.random.permutation(my_file.index) )
+
+    # return resampled sets
+    return tmp[ 0:int( train_size*len(my_file) ) ], tmp[ int( train_size*len(my_file) ): ]  
+
+
+def create_labeled_sentence_vectors(file,file2,file2_test):
 
     docs = []
     for index, row in file.iterrows():
@@ -27,7 +41,11 @@ def read_data():
     for index, row in file2.iterrows():
         docs2.append(LabeledSentence(words = gensim.utils.simple_preprocess(str(row["nome_cap"]) + ' ' + str(row["nome_grupo"]) + ' ' + str(row["nome_subgrupo"]) + ' ' + str(row["procedimento"]), deacc=True, min_len=3, max_len=301), tags = ['tuss_' + str(row["codigo"])]))
 
-    return docs, docs2
+    docs2_test = []
+    for index, row in file2_test.iterrows():
+        docs2_test.append(LabeledSentence(words = gensim.utils.simple_preprocess(str(row["nome_cap"]) + ' ' + str(row["nome_grupo"]) + ' ' + str(row["nome_subgrupo"]) + ' ' + str(row["procedimento"]), deacc=True, min_len=3, max_len=301), tags = ['tuss_' + str(row["codigo"])]))
+
+    return docs, docs2, docs2_test
 
 
 def create_model( docs2, num_features ):
@@ -98,25 +116,32 @@ def main():
     # parametros TODO dá pau se mudar pra menos
     num_features = 6000
 
+    resampling_rounds = 10
+
+
     # docs = conjunto de mapeamento (gold standard entre tuss e sigtap)
     # docs2 = categorização hierarquica do tuss
-    docs,docs2 = read_data()
-   
-    test_size = 1
+    file,file2 = read_data()
 
-    for r_round in range(1,2): # up to 90%
+   
+    for r_round in range(resampling_rounds): # up to 90%
+
+        train_file2, test_file2 = resample_input_file( file2, train_size=(1-1/resampling_rounds) )
+
+        #filename = "/tmp/file%d.xls"%(r_round)
+        #writer = pd.ExcelWriter(filename)
+        #train_file2.to_excel( writer, "Coisa 1"  )
+        #writer.save()
+
+        # TODO use docs2_test
+        docs,docs2,docs2_test = create_labeled_sentence_vectors(file , train_file2, test_file2)
+
+
         print( "%s Iniciando round %d de resampling (usando mytrain_test_split para debug-1)" % ( str(datetime.datetime.now()),r_round ) )
 
-        test_size /= 10
-        train_size = 1 - test_size
-
-        print("Resampling train_size=%d"%train_size)
-        docs2_train, docs2_test  = my_train_test_split( docs2, train_size=train_size )
-
-        print("docs2 = %d, train = %d"%(len(docs2), len(docs2_train)))
 
         # cria um modelo baseado nos dados do TUSS
-        model,dictionary,tfidf = create_model( docs2_train, num_features )
+        model,dictionary,tfidf = create_model( docs2, num_features )
 
         # realiza avaliação de acurácia do modelo gerado
         codigos_certos, cap_certos, grupo_certos, subgrupo_certos, resultado = evaluate( docs, docs2, model, dictionary, tfidf )
@@ -124,8 +149,8 @@ def main():
         print("Teste do modelo: Codigos;" + str(codigos_certos) + " Capitulo=" + str(cap_certos) + " Grupo=" + str(grupo_certos) + " SubGrupo=" + str(subgrupo_certos) )
         print("Teste do modelo: Codigos;" + str(codigos_certos/len(docs)) + " Capitulo=" + str(cap_certos/len(docs)) + " Grupo=" + str(grupo_certos/len(docs)) + " SubGrupo=" + str(subgrupo_certos/len(docs)) )
 
-        print(">%d%%,%d,%d,%d,%d,%d"%(r_round,codigos_certos,cap_certos,grupo_certos,subgrupo_certos,len(docs)))
 
+        print("SELF>%d%%,%d,%d,%d,%d,%d"%(r_round,codigos_certos,cap_certos,grupo_certos,subgrupo_certos,len(docs)))
     
 
 main()
